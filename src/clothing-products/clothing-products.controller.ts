2088a1d96dbd38c6869df3247d5680624ceb2ecc
P A Chathura Madhava <chathuratechdev@gmail.com>
2024-06-26 12:54:16 +0530

    Apply create product function

--- src/clothing-products/clothing-products.controller.ts
+++ src/clothing-products/clothing-products.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ClothingProductsService } from './clothing-products.service';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Controller('clothing-products')
 export class ClothingProductsController {
@@ -8,5 +9,11 @@ export class ClothingProductsController {
     @Get()
     public async getAllProducts() {
         return await this.clothingService.getAllProducts();
+    }
+
+    @Post()
+    public async createAProduct(@Body() createProductDto: CreateProductDto) {
+        // console.log(createProductDto);
+        return await this.clothingService.createAProduct(createProductDto);       
     }
 }
--- src/clothing-products/clothing-products.service.ts
+++ src/clothing-products/clothing-products.service.ts
@@ -1,19 +1,33 @@
 import { HttpService } from '@nestjs/axios';
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { firstValueFrom } from 'rxjs';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Injectable()
 export class ClothingProductsService {
-    constructor(private readonly httpService: HttpService) {}
+  constructor(private readonly httpService: HttpService) {}
 
-    async getAllProducts() {
-        try {
-            const response = await firstValueFrom(
-                this.httpService.get('https://fakestoreapi.com/products')
-            )
-            return response.data;            
-        } catch (error) {
-            throw new BadRequestException();
-        }
+  async getAllProducts() {
+    try {
+      const response = await firstValueFrom(
+        this.httpService.get('https://fakestoreapi.com/products'),
+      );
+      return response.data;
+    } catch (error) {
+      throw new BadRequestException();
     }
+  }
+
+  async createAProduct(createProductDto: CreateProductDto) {
+    // console.log(createProductDto);
+
+    try {
+      const response = await firstValueFrom(
+        this.httpService.post('https://fakestoreapi.com/products', createProductDto),
+      );
+      return response.data;
+    } catch (error) {
+      throw new BadRequestException();
+    }
+  }
 }
