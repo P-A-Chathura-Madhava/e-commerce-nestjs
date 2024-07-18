530cb07c5cfe75ce2ed91d16acc67f983111290a
P A Chathura Madhava <chathuratechdev@gmail.com>
2024-07-18 09:57:10 +0530

    Apply validation to createEnquiryDto

--- src/enquiries/dto/create-enquiry.dto.ts
+++ src/enquiries/dto/create-enquiry.dto.ts
@@ -1,9 +1,18 @@
+import { IsString } from "class-validator";
 import { EnquiryStatus } from "../entities/enquiry.entity";
 
 export class CreateEnquiryDto {
+    @IsString()
     name: string;
+
+    @IsString()
     email: string;
+
+    @IsString()
     mobile: string;
+
+    @IsString()
     comment: string;
+    
     status: EnquiryStatus;
 }
