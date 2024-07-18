b14ae640d24401e5317e90607b63cb41a3ca5a65
P A Chathura Madhava <chathuratechdev@gmail.com>
2024-07-18 09:56:09 +0530

    Apply validation to createCouponDto

--- src/coupons/dto/create-coupon.dto.ts
+++ src/coupons/dto/create-coupon.dto.ts
@@ -1,5 +1,12 @@
+import { IsDateString, IsNumber, IsString } from "class-validator";
+
 export class CreateCouponDto {
+    @IsString()
     name: string;
+
+    @IsDateString()
     expiry: Date;
+
+    @IsNumber()
     discount: number;
 }
